9e1f978d88529ab8e89e0615de8596cb547cf6c2
Aaron0521 <40079377+Aaron0521@users.noreply.github.com>
2020-09-09 18:18:29 +0800

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-
-if os.path.exists(r"C:\Users\aaron\PycharmProjects\filebeat\data\registry"):
-    shutil.rmtree(r"C:\Users\aaron\PycharmProjects\filebeat\data\registry")
-
-os.chdir(r"C:\Users\aaron\PycharmProjects\filebeat")
-os.system("filebeat -e -c filebeat.yml")
